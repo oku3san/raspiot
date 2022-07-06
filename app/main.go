3c4e0ff8ad55cd78d7fea9f031d985c84fd2ff79
oku3san <takaya0303@gmail.com>
2022-07-06 23:26:48 +0900

    標準出力に値を表示

--- app/main.go
+++ app/main.go
@@ -4,12 +4,62 @@ import (
   "fmt"
   "github.com/quhar/bme280"
   "golang.org/x/exp/io/i2c"
+  "time"
 )
 
+// BME280 デバイスの初期化
+func initBme280() (*bme280.BME280, error) {
+  dev, err := i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"}, 0x76)
+
+  if err != nil {
+    fmt.Println("OpenDevice error")
+    return nil, err
+  }
+
+  bme280 := bme280.New(dev)
+  err = bme280.Init()
+  return bme280, err
+}
+
+// BME280 デバイスのセンサーからイベントを取得して表示
+func outputSensorValues(bme *bme280.BME280) (err error) {
+  temperature, pressure, humidity, err := bme.EnvData()
+  if err != nil {
+    fmt.Println("EnvData error")
+    return
+  }
+
+  fmt.Printf("Temperature: %.2fC, Humidity: %.2f%%, Pressure: %.2fhpa, \n", temperature, humidity, pressure)
+  return
+}
+
 func main() {
-  dev, _ := i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"}, 0x76)
-  bme := bme280.New(dev)
-  bme.Init()
-  temperature, pressure, humidity, _ := bme.EnvData()
-  fmt.Println(temperature, pressure, humidity)
+  // インターバル
+  const outputIntervalSec = 60
+
+  // BME280 デバイスの初期化
+  bme280, err := initBme280()
+  if err != nil {
+    fmt.Println("Init error")
+    return
+  }
+
+  // 値取得間隔の設定
+  ticker := time.NewTicker(outputIntervalSec * time.Second)
+
+  // 終了したら Stop を実行する
+  defer ticker.Stop()
+
+  for {
+    // チャネルの送受信操作の多重化
+    select {
+    // ticker を利用して、一定間隔で繰り返し実行
+    case <-ticker.C:
+      err = outputSensorValues(bme280)
+      if err != nil {
+        fmt.Println("Output error")
+        return
+      }
+    }
+  }
 }
